e698b6ed0083db4339c8694a15945786d5edd4ac
Wade Wegner <wade.wegner@gmail.com>
2013-11-25 19:28:13 -0700

    New iOS project

---
+++ src/LoginApp/LoginApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LoginApp
+//
+//  Created by Wade Wegner on 11/25/13.
+//  Copyright (c) 2013 Wade Wegner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ src/LoginApp/LoginApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LoginApp
+//
+//  Created by Wade Wegner on 11/25/13.
+//  Copyright (c) 2013 Wade Wegner. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
